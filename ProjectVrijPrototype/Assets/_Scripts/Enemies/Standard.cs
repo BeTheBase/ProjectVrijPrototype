fb66d0a44f15b4ab1d14f51bc851b7a1b7f6f4a2
reinier303 <30725987+reinier303@users.noreply.github.com>
2019-04-25 16:08:43 +0200

    Added scripts for targeting testing

---
+++ ProjectVrijPrototype/Assets/_Scripts/Enemies/Standard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Standard : MonoBehaviour
+{
+    public float MaxHealth;
+
+    public float Health;
+
+    public int GoldGiven;
+
+    private NavMeshAgent agent;
+
+    private EnemySpawner enemySpawner;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponentInChildren<NavMeshAgent>();
+        enemySpawner = EnemySpawner.Instance;
+        gameManager = GameManager.Instance;
+        agent.SetDestination((gameManager.EndPosition));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y <= -5)
+        {
+            enemySpawner.EnemiesAlive--;
+            gameObject.SetActive(false);
+        }
+        if(Health <= 0)
+        {
+            enemySpawner.EnemiesAlive--;
+            gameManager.Gold += GoldGiven;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "End")
+        {
+            enemySpawner.EnemiesAlive--;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ ProjectVrijPrototype/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Vector3 EndPosition;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Text GoldText;
+
+    public int Gold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GoldText.text = "Gold:" + Gold;
+    }
+}
