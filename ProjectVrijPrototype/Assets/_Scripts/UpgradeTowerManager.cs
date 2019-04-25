59ae6d40774733fdeea1a18eaebf2de35246d94d
Bas <Bas.ghosts@gmail.com>
2019-04-25 16:13:29 +0200

    Turret Models and UI

---
+++ ProjectVrijPrototype/Assets/_Scripts/UpgradeTowerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeTowerManager : MonoBehaviour
+{
+    public Mesh ArrowTowerUpgradeMesh;
+    public Mesh BombTowerUpgradeMesh;
+    public Mesh MagicTowerUpgradeMesh;
+
+    private GameObject tower;
+
+    public void UpgradeTower()
+    {
+        tower = this.transform.parent.gameObject;
+        switch(tower.gameObject.tag)
+        {
+            case "ArrowTower":
+                tower.GetComponent<MeshFilter>().mesh = ArrowTowerUpgradeMesh;
+                break;
+            case "BombTower":
+                tower.GetComponent<MeshFilter>().mesh = BombTowerUpgradeMesh;
+                break;
+            case "MagicTower":
+                tower.GetComponent<MeshFilter>().mesh = MagicTowerUpgradeMesh;
+                break;
+            default:
+                tower.GetComponent<MeshFilter>().mesh = BombTowerUpgradeMesh;
+                break;
+        }
+
+    }
+
+    public void DeleteTower()
+    {
+        tower = this.transform.parent.gameObject;
+        Destroy(tower);
+    }
+}
