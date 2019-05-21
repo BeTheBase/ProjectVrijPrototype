d6b601a6da491d9b5ddd1d614af20e9fd39c238a
reinier303 <30725987+reinier303@users.noreply.github.com>
2019-05-21 22:28:37 +0200

    Fixed Menu

--- ProjectVrijPrototype/Assets/_Scripts/Managers/MenuManager.cs
+++ ProjectVrijPrototype/Assets/_Scripts/Managers/MenuManager.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject LoadingWheel;
+
+    public void Play()
     {
-        
+        StartCoroutine(LoadingScreen());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadingScreen()
     {
-        
+        EventSystem.current.enabled = false;
+        LoadingWheel.SetActive(true);
+        yield return new WaitForSeconds(3);
+
+        while (!SceneManager.LoadSceneAsync(1).isDone)
+        {
+            print("f");
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
